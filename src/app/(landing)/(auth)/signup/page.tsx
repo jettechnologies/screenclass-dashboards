0cb1008ca87e1b90df4f824c066187d0ad99341b
jettechnologies <tomesitime04@gmail.com>
2025-04-01 15:36:36 +0100

    fix(ui): fixed the signup responsive design

--- src/app/(landing)/(auth)/signup/page.tsx
+++ src/app/(landing)/(auth)/signup/page.tsx
@@ -6,10 +6,10 @@ import {
 export default function page() {
   return (
     <div className="flex min-h-screen w-full flex-col gap-x-10 text-white lg:flex-row">
-      <div className="min-h-[350px] w-full flex-1 border-2 border-black py-[3rem] lg:min-h-screen">
+      <div className="min-h-[350px] w-full flex-1 py-[3rem] lg:min-h-screen">
         <SignupContent />
       </div>
-      <div className="-mt-[20%] h-full w-full flex-1 place-items-center px-8 pb-8 lg:-mt-0 lg:px-14 lg:py-12">
+      <div className="-mt-[20%] h-full w-full flex-1 place-items-center px-8 pb-8 md:-mt-[10%] lg:-mt-0 lg:px-14 lg:py-12">
         <SignupForm />
       </div>
     </div>
