28f4eb5c2d39805106b7f1ca16fe0d1d070b8315
jettechnologies <tomesitime04@gmail.com>
2025-04-01 15:58:41 +0100

    refactor: remove unused usestate

--- src/components/modal/guardian/StudentOverviewModal.tsx
+++ src/components/modal/guardian/StudentOverviewModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Modal from "react-modal";
 import { nunito } from "../../shared/fonts";
 // import ManageStudentsModal from "./ManageStudentsModal";
